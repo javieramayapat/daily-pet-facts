fa5f773f04a3e4f63ff33819ffe6dac40a1a4805
Javier Amaya <Javieramayapat@gmail.com>
2022-07-15 00:17:48 -0500

    [feat] Add pets path operations to list all pets and get one pet by id

--- app/routers/pets_router.py
+++ app/routers/pets_router.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, HTTPException, Path, status
+from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/pets", tags=["Pets"])
+from ..dependencies import get_db
+from ..schemas import pet_schema
+from ..services import pet_service
+
+router = APIRouter(prefix="/pets",
+                   tags=["Pets"],
+                   dependencies=[Depends(get_db)])
 
 
-@router.get(path="/", status_code=status.HTTP_200_OK)
-def get_pet_types():
-    pass
+@router.get(path="/", response_model=list[pet_schema.PetOut], status_code=status.HTTP_200_OK)
+def get_pet_types(db: Session = Depends(get_db)):
+    pets = pet_service.get_pets(db=db)
+    return pets
+
+
+@router.get(path="/{pet_id}", response_model=pet_schema.PetOut, status_code=status.HTTP_200_OK)
+def get_pet(pet_id: int = Path(..., gt=0, example=1), db: Session = Depends(get_db)):
+    pet = pet_service.get_pet(db=db, pet_id=pet_id)
+    if pet is None:
+        raise HTTPException(status_code=404, detail="Pet not found")
+    return pet
